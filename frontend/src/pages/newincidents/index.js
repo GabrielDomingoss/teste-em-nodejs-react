f41c5c9236fe8b98ec5df8aa877ddfc4116da217
Gabriel Domingos Pinheiro de Sousa <gabriel.sousa@axxess.com.br>
2020-06-03 15:56:47 -0400

    Front-End do app

--- frontend/src/pages/newincidents/index.js
+++ frontend/src/pages/newincidents/index.js
@@ -1,10 +1,34 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React,{ useState} from 'react';
+import {Link , useHistory} from 'react-router-dom';
 import {FiArrowLeft} from 'react-icons/fi';
 import logoImg from '../../assets/logo.svg';
+import api from '../../services/api';
 import './style.css';
 
 export default function NewIncident(){
+    const[title,setTitle] = useState('');
+    const[description,setDescription] = useState('');
+    const[value,setValue] = useState('');
+    const history = useHistory();
+    const ongId = localStorage.getItem('ongId');
+    async function handleNewIncident(e){
+        e.preventDefault();
+        const data= {
+            title,
+            description,
+            value
+        };
+        try{
+            await api.post('incidents',data,{
+                headers:{
+                    Authorization:ongId,
+                }
+            })
+            history.push('/profile');
+        }catch(err){
+            alert('Erro ao cadastrar, tente novamente');
+        }
+    }
     return(
         <div className="new-incident-container">
             <div className="content">
@@ -15,11 +39,11 @@ export default function NewIncident(){
                     <Link className="back-link" to="/profile"><FiArrowLeft size={16} color="#E02041"/>Voltar para a home</Link>
                 </section>
                 <form>
-                    <input placeholder="titulo do caso"></input>
-                    <textarea placeholder="Descrição"></textarea>
-                    <input placeholder="valor em reais"></input>
+                    <input value={title} onChange={ e=> setTitle(e.target.value)} placeholder="titulo do caso"></input>
+                    <textarea value={description} onChange={ e=> setDescription(e.target.value)} placeholder="Descrição"></textarea>
+                    <input value={value} onChange={ e=> setValue(e.target.value)} placeholder="valor em reais"></input>
 
-                    <button className="button">Cadastrar</button>
+                    <button onClick={handleNewIncident} className="button">Cadastrar</button>
                 </form>
             </div>
         </div>
--- frontend/src/pages/register/index.js
+++ frontend/src/pages/register/index.js
@@ -1,9 +1,34 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React,{useState} from 'react';
+import {Link , useHistory} from 'react-router-dom';
 import {FiArrowLeft} from 'react-icons/fi';
+import api from '../../services/api';
 import './style.css';
 import logoImg from '../../assets/logo.svg';
 export default function Register(){
+    const [name,setName] = useState('');
+    const [email,setEmail] = useState('');
+    const [whatsapp,setWhatsapp] = useState('');
+    const [city,setCity] = useState('');
+    const [uf,setUf] = useState('');
+    const history = useHistory();
+    async function handleRegister(e){
+        e.preventDefault();
+        const data = {
+            name,
+            email,
+            whatsapp,
+            city,
+            uf
+        };
+        try{
+            const response = await api.post('ongs',data);
+            alert(`Seu ID de acesso: ${response.data.id}`);
+            history.push('/');
+        }catch (err){
+            alert('Erro no cadastro, tente novamente');
+        }
+       
+    }
     return(
         <div className="register-container">
             <div className="content">
@@ -11,16 +36,16 @@ export default function Register(){
                     <img src={logoImg} alt="Be the Hero"/>
                     <h1>Cadastro</h1>
                     <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontratem os casos da sua ONG</p>
-                    <Link className="back-link" to="/"><FiArrowLeft iLosize={16} color="#E02041"/>Nao tenho cadastro</Link>
+                    <Link className="back-link" to="/"><FiArrowLeft size={16} color="#E02041"/>Nao tenho cadastro</Link>
                 </section>
-                <form>
-                     <input placeholder="Nome da ONG"></input>
-                     <input type="email" placeholder="email"></input>
-                     <input placeholder="WhatsApp"></input>
+                <form onSubmit= {handleRegister}>
+                     <input placeholder="Nome da ONG" value = {name} onChange={ e=> setName(e.target.value)}></input>
+                     <input type="email" placeholder="email"  value = {email} onChange={ e=> setEmail(e.target.value)}></input>
+                     <input placeholder="WhatsApp"  value = {whatsapp} onChange={ e=> setWhatsapp(e.target.value)}></input>
 
                      <div className="input-group">
-                         <input placeholder="cidade"></input>
-                         <input placeholder = "UF" style = {{width:80}}></input>
+                         <input placeholder="cidade"  value = {city} onChange={ e=> setCity(e.target.value)}></input>
+                         <input placeholder = "UF" style = {{width:80}}  value = {uf} onChange={ e=> setUf(e.target.value)}></input>
                      </div>
                      <button className="button">Cadastrar</button>
                 </form>
